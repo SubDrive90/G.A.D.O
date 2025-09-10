cb8b9a27df8c2743a833d318ba5a33521b1ec435
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-10 18:09:59 +0000

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # G.A.D
 um assitente virtual em python
+
+Foco: 
+    facil de aprender.
+    rapido e expansivel.
+
+Técnologia:
+    Reconhecimento de voz.
+    sintese de voz.
+    Algum de tipo IA.
---
+++ main.py
@@ -0,0 +1,13 @@
+# our main file
+
+import speech_recognition as sr
+
+# cria um reconhecedor
+r = sr.Recognizer()
+
+#abrir microfone para captura 
+
+with sr.Microphone() as source:
+    audio = r.listen(source) # define microfone como fonte de audio
+
+    print(r.recognize_google(audio))
