7576f994847bc1c31659dc0e9bba9b24ceb967a1
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-10 18:17:05 +0000

    olá

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source) # define microfone como fonte de audio
 
-    print(r.recognize_google(audio))
+    print(r.recognize_google(audio, language="pt-BR"))
