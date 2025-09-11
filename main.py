54a905ec5a3a8082fcef277c8fc46b53d3d712b6
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-11 14:19:30 +0000

    update 6

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import speech_recognition as sr 
 import pyAudio
 import pyttsx3
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from my_window_ui import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        #inicialização da interface
+        self.setupUi(self)
+
+        #Conecta o sinal 'clicked' do botão a uma função (slot)
+        #O nome do botão é 'pushButton', que foi o nome padrão entrege pelo designer
+        self.pushButton.clicked.connect(self.on_button_clicked)
+    
+    def on_button_clicked(self):
+        """ Função (slot) que será executada quando o botão for clicado."""
+        QMessageBox.information(self,"Ação", "Você clicou no botão")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
 #sistema de fala
 engine = pyttsx3.init()
 
@@ -28,7 +53,7 @@ def gemma_response(user_text):
 with sr.Microphone() as source:
     print("Ajustando para o ruido ambiente...")
     r.adjust_for_ambient_noise(source, duration=5)
-    print(Pronto! Pode falar agora:)
+    print("Pronto Pode falar agora:")
 
     audio = r.listen(source)
 
@@ -55,3 +80,4 @@ with sr.Microphone() as source:
         print("Erro ao se conectar com o serviço de reconhecimento.")
 
     #pip install pyttsx3
+    #pip install pyqt6 pyqt6-tools
