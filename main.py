f62b595a7f7e7a4cf0356f7fc34872d0475a1348
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-11 18:12:01 +0000

    update 8

--- main.py
+++ main.py
@@ -1,83 +1,21 @@
-import speech_recognition as sr 
-import pyAudio
-import pyttsx3
-import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
-from my_window_ui import Ui_MainWindow
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
+model = Model("model")
+rec = KaldiRecognizer(model, 16000)
 
-        #inicialização da interface
-        self.setupUi(self)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
 
-        #Conecta o sinal 'clicked' do botão a uma função (slot)
-        #O nome do botão é 'pushButton', que foi o nome padrão entrege pelo designer
-        self.pushButton.clicked.connect(self.on_button_clicked)
-    
-    def on_button_clicked(self):
-        """ Função (slot) que será executada quando o botão for clicado."""
-        QMessageBox.information(self,"Ação", "Você clicou no botão")
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
-
-#sistema de fala
-engine = pyttsx3.init()
-
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[-3].id)
-
-
-def speak(text):
-    """ Fala o texto em voz alta"""
-    engine.say(text)
-    engine.runAndWait()
-
-r = sr.Recognizer()
-
-#atribuição da IA
-def gemma_response(user_text):
-    """ 
-    Aqui enviarei user_text para o modelo Gemma 2.0B
-    e receberia a resposta. 
-    """
-    resposta = modelo_gerador_resposta(user_text)
-    return resposta
-
-#ajusta o ruido ao redor
-with sr.Microphone() as source:
-    print("Ajustando para o ruido ambiente...")
-    r.adjust_for_ambient_noise(source, duration=5)
-    print("Pronto Pode falar agora:")
-
-    audio = r.listen(source)
-
-
-    try:
-        #converte o Audio para texto
-        user_input = r.recognize_google(audio, language='pt-BR')
-        print(user_input)
-
-        print("Estou pensando")
-        pyttsx3.say("Estou pensando...")
-        pyttsx3.runAndWait()
-        #obtem a resposta da IA
-        resposta = gemma_response(user_input)
-        print("Assistente:", resposta)
-        
-        #fala a resposta
-        pyttsx3.say(resposta)
-        pyttsx3.runAndWait()
-
-    except sr.UnKnownValueErroe:
-        print("Não entendi o que você disse.")
-    except sr.RequestError:
-        print("Erro ao se conectar com o serviço de reconhecimento.")
-
-    #pip install pyttsx3
-    #pip install pyqt6 pyqt6-tools
+print(rec.FinalResult())
