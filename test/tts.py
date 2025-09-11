80efef5d635d9f6403c91740223e53525e71212c
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-11 19:36:24 +0000

    update

--- test/tts.py
+++ test/tts.py
@@ -2,8 +2,9 @@ import pyttsx3
 engine = pyttsx3.init()
 
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice'. voices[-3].id)
+voices = enine.getproperty('voices')
+
+engine.setProperty('voice',voices[-2].id)
 
 engine.say("Eu vou falar esse texto")
 engine.runAndWait()
