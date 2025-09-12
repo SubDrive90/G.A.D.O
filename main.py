7e6ff5bdc0a711331f01a81c2f9674fe9e3db1e2
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-12 13:56:50 +0000

    Implementação da IA

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ import os
 import pyaudio
 import pyttsx3
 import json
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+model_id = "google/gemma-2b-it"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16
+)
+
+def get_gemma_response(prompt_text):
+
+    chat_prompt = f"O usuario diz: {prompt_text}. Responda á pergunta do usuario."
+
+    input_ids = tokenizer(chat_prompt, return_tensors="pt")
+
+    outputs = model.generate(**input_ids, max_new_tokens=150)
+
+    response = tokenizer.decode(outputs[0])
+
+    response = response.split(chat_prompt)[-1].strip()
+
+    return response
 
 #sintese de fala
 engine = pyttsx3.init()
@@ -27,13 +51,16 @@ while True:
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        result = rec.Result()
-        result = json.loads(result)
+        result = json.loads(rec.Result())
+        text = result['text']
+        if text:
+            print(f"Você disse: {text}")
 
-        if result is not None:
-            text = result['text']
+            ia_response = get_gemma_response(text)
 
-            print(text)
-            speak(text)
+            print(f"IA respondeu: {ia_response}")
+            speak(ia_response)
+
 
 #https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip
+#pip install transformers accelerate
