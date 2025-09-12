c27b010e7b6d164e17bd5b9f579c01cc127b0dcf
SubDrive90 <gabrielgoncalvespereira60@gmail.com>
2025-09-12 14:17:29 +0000

    update

--- main.py
+++ main.py
@@ -43,16 +43,27 @@ model = Model("model")
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
-stream.start_stream()
 
-while True:
-    data = stream.read(4000,exception_on_overflow=False)
-    if len(data) == 0:
-        break
+try:
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+    stream.start_stream()
+    print("Ouvindo... Pressione Ctrl+C para sair.")
+except OSError as e:
+    print(f"Erro inesperado: {e}")
+    speak("Desculpe Ocorreu um erro! vou ter que reiniciar.")
+    exit()
+
+
+try:
+    while True:
+        data = stream.read(4000,exception_on_overflow=False)
+        if len(data) == 0:
+            break
+
     if rec.AcceptWaveform(data):
         result = json.loads(rec.Result())
         text = result['text']
+
         if text:
             print(f"Você disse: {text}")
 
@@ -60,7 +71,9 @@ while True:
 
             print(f"IA respondeu: {ia_response}")
             speak(ia_response)
-
+except Exception as e:
+    print(f"Erro inesperado: {e}")
+    speak("Desculpe ")
 
 #https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip
 #pip install transformers accelerate
